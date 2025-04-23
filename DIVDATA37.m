02b0563728e58662f0a97b40f685503299ec9433
zongtingwei <20228131062@m.scnu.edu.cn>
2025-04-23 21:49:50 +0800

    Update DIVDATA37.m

--- DIVDATA37.m
+++ DIVDATA37.m
@@ -4,22 +4,13 @@ load(file)
 
 dataMat = ins;
 len = size(dataMat, 1);
-% 归一化
 maxV = max(dataMat);
 minV = min(dataMat);
 range = maxV - minV;
 newdataMat = (dataMat - repmat(minV, [len, 1])) ./ (repmat(range, [len, 1]));
-
-% 获取数据集的标签
 lab = lab;
-
-% 随机打乱数据集索引
 indices = randperm(length(lab));
-
-% 计算训练集和测试集的索引分割点
 splitIdx = round(0.7 * length(lab)); % 70% for training
-
-% 根据索引分割训练集和测试集
 trainIdx = indices(1:splitIdx);
 testIdx = indices(splitIdx+1:end);
 
